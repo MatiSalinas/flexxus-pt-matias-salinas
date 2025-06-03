c0c78e95c541f633073174b6dde4f4b148331eeb
Salinas Matias <salinasmatiasit@gmail.com>
2025-06-02 22:23:11 -0300

    proyecto dockerizado, ya no es necesario crear manualmente la base de datos

---
+++ docs/db/insertar_roles.sql
@@ -0,0 +1,2 @@
+INSERT INTO `flexxus_prueba_tecnica`.`roles` (`id_rol`, `nombre_rol`) VALUES ('1', 'usuario');
+INSERT INTO `flexxus_prueba_tecnica`.`roles` (`id_rol`, `nombre_rol`) VALUES ('2', 'administrador');
